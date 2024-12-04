91cd91a50bd9b15b083eeab49283dcc49640785e
Andaka Ramdhani Santoso <andaka@goodeva.co.id>
2024-12-04 21:04:26 +0700

    first commit

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CategoryDocument;
+use App\Models\CategoryKajian;
+use App\Models\Services;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function homepage(){
+        $data['category'] = CategoryDocument::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->get();
+        $data['slider'] = Slider::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->get();
+        $data['product'] = Services::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->limit(5)->get();
+        $data['allProducts'] = Services::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->get();
+        return view('pwa.home.index',$data);
+    }
+    public function detail($id){
+        $data['product'] = Services::whereNull('deleted_at')->where('status',1)->orderBy('created_at','desc')->limit(5)->whereNotIn('id',[$id])->get();
+        $data['prod'] = Services::find($id);
+        $data['page_title'] = $data['prod']->service;
+        return view('pwa.home.detail',$data);
+    }
+}
